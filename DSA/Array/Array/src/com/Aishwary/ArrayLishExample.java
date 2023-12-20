75447ee065607cb64650734250ccbe9c47ff5044
Aishwary Gupta <115387744+Aishwary2004Gupta@users.noreply.github.com>
2023-12-20 10:54:54 +0530

    Add files via upload

---
+++ DSA/Array/Array/src/com/Aishwary/ArrayLishExample.java
@@ -0,0 +1,37 @@
+package com.Aishwary;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayLishExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        ArrayList <Integer> list = new ArrayList<>(5);
+
+//        list.add(71);
+//        list.add(19);
+//        list.add(11);
+//
+//        System.out.println(list.contains(12));
+//        System.out.println(list);
+//        list.set(0,99); //Change
+//        list.remove(2);
+//        System.out.println(list);
+
+        //Input
+
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+
+        }
+
+        //get item at any index
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i)); //pass index here, list[index] will not work here
+        }
+
+        System.out.println(list);
+
+    }
+}
